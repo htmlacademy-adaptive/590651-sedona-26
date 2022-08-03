5827b4dc49b16a5f78a75333884de1f46f977068
Alex Gorodov <a.gorodov.22@gmail.com>
2022-08-03 13:25:30 +0300

    index page style added

--- source/js/script.js
+++
@@ -1,6 +0,0 @@
-const burgerToggle = document.querySelector('.navigation-toggle');
-const mobileNav = document.querySelector('.navigation__list');
-
-burgerToggle.addEventListener('click', () => {
-  mobileNav.classList.toggle('.navigation__list--opened');
-});
