d22c1d60b46b04aa25bb45c708aa72265544300c
Alex Gorodov <a.gorodov.22@gmail.com>
2022-08-07 21:37:59 +0300

    work on adaptive started

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,10 +1,12 @@
 // Burger menu toggle
 const burgerToggle = document.querySelector('.navigation-toggle');
 const burgerToggleLines = document.querySelectorAll('.navigation-toggle__line');
-const mobileNav = document.querySelector('.navigation__list');
+const mobileNav = document.querySelectorAll('.navigation__list');
 
 burgerToggle.addEventListener('click', () => {
-  mobileNav.classList.toggle('navigation__list--opened');
+  mobileNav.forEach(element => {
+    element.classList.toggle('navigation__list--opened');
+  });
   burgerToggleLines[0].classList.toggle('navigation-toggle__line-first--open');
   burgerToggleLines[1].classList.toggle('navigation-toggle__line-second--open');
   burgerToggleLines[2].classList.toggle('navigation-toggle__line-third--open');
