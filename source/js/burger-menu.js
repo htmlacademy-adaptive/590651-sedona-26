3b9b82a0e2b7a33237713469b419966521630968
Alex Gorodov <a.gorodov.22@gmail.com>
2022-08-09 01:39:28 +0300

    fix nav menu

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,19 +1,18 @@
 // Burger menu toggle
 const burgerToggle = document.querySelector('.navigation-toggle');
 const burgerToggleLines = document.querySelectorAll('.navigation-toggle__line');
-const mobileNav = document.querySelectorAll('.navigation__list');
+const mobileNav = document.querySelector('.navigation__wrapper');
 
-mobileNav.forEach(element => {
-  element.classList.remove('navigation__list--nojs');
-});
+mobileNav.classList.remove('navigation__wrapper--nojs');
+
 burgerToggleLines[0].classList.remove('navigation-toggle__line-first--open');
 burgerToggleLines[1].classList.remove('navigation-toggle__line-second--open');
 burgerToggleLines[2].classList.remove('navigation-toggle__line-third--open');
 
 burgerToggle.addEventListener('click', () => {
-  mobileNav.forEach(element => {
-    element.classList.toggle('navigation__list--opened');
-  });
+
+  mobileNav.classList.toggle('navigation__wrapper--opened');
+
   burgerToggleLines[0].classList.toggle('navigation-toggle__line-first--open');
   burgerToggleLines[1].classList.toggle('navigation-toggle__line-second--open');
   burgerToggleLines[2].classList.toggle('navigation-toggle__line-third--open');
