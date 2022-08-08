531573b336c01c4194385fef9a6d6a4ed263ea9d
Keks <keks@htmlacademy.ru>
2022-08-09 01:24:39 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/burger-menu.js
+++ 6/js/burger-menu.js
@@ -3,6 +3,13 @@ const burgerToggle = document.querySelector('.navigation-toggle');
 const burgerToggleLines = document.querySelectorAll('.navigation-toggle__line');
 const mobileNav = document.querySelectorAll('.navigation__list');
 
+mobileNav.forEach(element => {
+  element.classList.remove('navigation__list--nojs');
+});
+burgerToggleLines[0].classList.remove('navigation-toggle__line-first--open');
+burgerToggleLines[1].classList.remove('navigation-toggle__line-second--open');
+burgerToggleLines[2].classList.remove('navigation-toggle__line-third--open');
+
 burgerToggle.addEventListener('click', () => {
   mobileNav.forEach(element => {
     element.classList.toggle('navigation__list--opened');
