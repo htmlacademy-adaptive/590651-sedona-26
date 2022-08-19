c5ca92d687c68ca3c9cf09a621d91848e33e2796
Alex Gorodov <a.gorodov.22@gmail.com>
2022-08-19 22:37:17 +0300

    postcss-csso and gulp-rename installed, minificated css created

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -14,6 +14,12 @@ burgerToggle.addEventListener('click', () => {
 
   mobileNav.classList.toggle('navigation__wrapper--opened');
 
+  if (mobileNav.classList.contains('navigation__wrapper--opened')) {
+    burgerToggle.setAttribute('aria-label', 'Закрыть меню.');
+  } else {
+    burgerToggle.setAttribute('aria-label', 'Открыть меню.');
+  }
+
   burgerToggleLines[0].classList.toggle('navigation-toggle__line-first--open');
   burgerToggleLines[1].classList.toggle('navigation-toggle__line-second--open');
   burgerToggleLines[2].classList.toggle('navigation-toggle__line-third--open');
